ca50b479acd64cd58ba8b1c8960b9e457dea1214
Limacite <marinejewelblue@gmail.com>
2021-01-18 02:00:21 +0900

    現在地を取得し地図を表示

--- src/map.js
+++ src/map.js
@@ -1,13 +1,22 @@
 let map;
-function initMap() {
+function initMap(lat,lng) {
   map = new google.maps.Map(document.getElementById("mapArea"),{
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+    center: { lat: lat, lng: lng },
+    zoom: 15,
     mapTypeId :"roadmap"
   });
 }
 
 window.onload = function(){
-    console.log("asafaf");
-    initMap();
+  navigator.geolocation.getCurrentPosition(function(position) {
+      //position から緯度経度（ユーザーの位置）のオブジェクトを作成し変数に代入
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      console.log("lat:" + pos.lat + "lng:" + pos.lng );
+      console.log("Map Complete");
+      initMap(pos.lat,pos.lng);
+
+    });
 }
