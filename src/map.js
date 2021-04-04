7423edf2e6d11104763901ec5b39e5bd0477342e
Limacite <marinejewelblue@gmail.com>
2021-04-04 20:00:46 +0900

    temp

--- src/map.js
+++ src/map.js
@@ -1,22 +1,52 @@
 let map;
-function initMap(lat,lng) {
+let marker = null;
+let pos  = {lat:0,lng:0};
+
+function initMap(latlng) {
   map = new google.maps.Map(document.getElementById("mapArea"),{
-    center: { lat: lat, lng: lng },
+    center: latlng ,
     zoom: 15,
     mapTypeId :"roadmap"
   });
+
+  map.addListener('click', function(e){
+      if (marker == null) {
+          marker = new google.maps.Marker({
+            position: e.latLng,
+            map: map,
+            title: "🐡",
+            animation: google.maps.Animation.DROP
+          });
+      }else {
+          marker.setMap(null);
+          marker = null;
+          marker = new google.maps.Marker({
+            position: e.latLng,
+            map: map,
+            title: "🐡",
+            animation: google.maps.Animation.DROP
+          });
+      }
+
+
+  });
+
 }
 
 window.onload = function(){
   navigator.geolocation.getCurrentPosition(function(position) {
       //position から緯度経度（ユーザーの位置）のオブジェクトを作成し変数に代入
-      var pos = {
+      pos = {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
       console.log("lat:" + pos.lat + "lng:" + pos.lng );
       console.log("Map Complete");
-      initMap(pos.lat,pos.lng);
+      initMap(pos);
 
     });
 }
+
+window.appEventListner("ps",function(){
+  app.ports.receivePosition.send(pos);
+});
